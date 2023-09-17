b307ae1d26741dd079c27b479e19188bbde7eec2
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-09-17 10:15:08 +0000

    deploy

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @random = PostImage.order("RANDOM()").limit(1)
+    random = Rails.env.production? ?  "rand()" : "RANDOM()"
+    @random = PostImage.order(random).limit(1)
     @post_images = PostImage.all.order(created_at: :desc).limit(3)
     @all_ranks = PostImage.find(Bookmark.group(:post_image_id).order('count(post_image_id) desc').limit(3).pluck(:post_image_id))
   end
